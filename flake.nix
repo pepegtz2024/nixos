ea3860ca53d5267925831b82702bd452ac25b82f
Jose de Jesus Gutierrez Campos <pepegtz2024@icloud.com>
2025-05-20 10:51:04 -0600

    Rebase to 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 	description = "La configuracion del sistema";
 	inputs = {
 		nixpkgs = {
-			url = "github:nixos/nixpkgs/nixos-unstable";
+			url = "github:nixos/nixpkgs/nixos-25.05";
 		};
 		nix-flatpak = {
 		url = "github:gmodena/nix-flatpak/?ref=latest";
