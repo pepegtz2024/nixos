eb5036c1e8c87509f3ae252862f8a35a21102587
Jose de Jesus Gutierrez Campos <pepegtz2024@icloud.com>
2025-05-21 22:22:11 -0600

    The authentication Finally Worked

--- flake.nix
+++ flake.nix
@@ -4,12 +4,15 @@
 		nixpkgs = {
 			url = "github:nixos/nixpkgs/nixos-25.05";
 		};
+
 		nix-flatpak = {
 		url = "github:gmodena/nix-flatpak/?ref=latest";
 		};
+
 		lanzaboote = {
         url = "github:nix-community/lanzaboote/v0.4.2";
         };
+
 		nixos-hardware = {
 		url = "github:NixOS/nixos-hardware/master";
 		};
