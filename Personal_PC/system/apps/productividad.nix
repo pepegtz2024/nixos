9eabd9d4a2b127f6e3b837765d39bd8d7f119bba
Jose de Jesus Gutierrez Campos <pepegtz2024@icloud.com>
2026-05-05 17:47:22 -0600

    blah

--- Personal_PC/system/apps/productividad.nix
+++ Personal_PC/system/apps/productividad.nix
@@ -1,6 +1,11 @@
 {
 services.flatpak.packages = [
 "com.obsproject.Studio"
+    "com.obsproject.Studio.Plugin.OBSVkCapture"
+    "com.obsproject.Studio.Plugin.DroidCam"
+    "com.obsproject.Studio.Plugin.BackgroundRemoval"
+    "com.obsproject.Studio.Plugin.WaylandHotkeys"
+    "com.obsproject.Studio.Plugin.OBSLivesplitOne"
 "org.libreoffice.LibreOffice"
 "org.mozilla.Thunderbird"
 "org.audacityteam.Audacity"
